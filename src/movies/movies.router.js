b836d03a89680d6c7094a442d88091777f509e23
Lev_Gordon <lev.gordonsa@gmail.com>
2022-08-08 18:51:15 -0700

    enabled cors

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -1,6 +1,9 @@
 const router = require("express").Router({mergeParams: true})
 const controller = require("./movies.controller")
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors")
+
+router.use(cors())
 
 router.route("/:movieId/theaters").get(controller.listTheatersHostingMovie).all(methodNotAllowed);
 router.route("/:movieId/reviews").get(controller.listReviewsAndCritics).all(methodNotAllowed);
